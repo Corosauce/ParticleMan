47dc373908219b5efcb94094a9cbaa82bde27970
Corosus <Cordonfreeman@gmail.com>
2013-03-29 19:41:47 -0400

    auto commit

--- particleman/entities/EntityParticleControllable.java
+++ particleman/entities/EntityParticleControllable.java
@@ -9,10 +9,17 @@ public class EntityParticleControllable extends Entity {
 	public String owner = "";
 	public int type = 0; //0 = fire, 1 = redstone
 	
+	public int decayTime = 0;
+	public int decayTimeMax = 80;
 	
 	public EntityParticleControllable(World par1World) {
 		super(par1World);
-		// TODO Auto-generated constructor stub
+	}
+	
+	public EntityParticleControllable(World par1World, String parOwner, int parType) {
+		super(par1World);
+		type = parType;
+		owner = parOwner;
 	}
 
 	@Override
@@ -20,6 +27,45 @@ public class EntityParticleControllable extends Entity {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public void influenceParticle(float parMotionX, float parMotionY, float parMotionZ) {
+		decayTime = 0;
+		motionX += parMotionX;
+		motionY += parMotionY;
+		motionZ += parMotionZ;
+	}
+	
+	@Override
+	public void onUpdate()
+    {
+		super.onUpdate();
+		
+		//Server logic
+		if (!worldObj.isRemote) {
+
+			decayTime++;
+			
+			if (decayTime > decayTimeMax) {
+				setDead();
+			}
+			
+			
+		}
+		
+		//Movement
+		float speedSlowing = 0.98F;
+		float gravity = 0F;
+		
+		this.motionX *= (double)speedSlowing;
+        this.motionY *= (double)speedSlowing;
+        this.motionZ *= (double)speedSlowing;
+        this.motionY -= (double)gravity;
+        
+        this.posX += this.motionX;
+        this.posY += this.motionY;
+        this.posZ += this.motionZ;
+		
+    }
 
 	@Override
 	protected void readEntityFromNBT(NBTTagCompound nbttagcompound) {
