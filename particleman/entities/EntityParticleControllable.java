dc2bd712a91962dff7073e3bb84be4a3880a4d06
Corosus <Cordonfreeman@gmail.com>
2013-03-29 18:26:47 -0400

    auto commit

---
+++ particleman/entities/EntityParticleControllable.java
@@ -0,0 +1,32 @@
+package particleman.entities;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public class EntityParticleControllable extends Entity {
+
+	public EntityParticleControllable(World par1World) {
+		super(par1World);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void entityInit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound nbttagcompound) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound nbttagcompound) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
