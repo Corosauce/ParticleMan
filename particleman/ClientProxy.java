dc13baf7e0b5400fb92d8422a8c2af573b2316e1
Corosus <Cordonfreeman@gmail.com>
2013-03-29 17:51:39 -0400

    auto commit

---
+++ particleman/ClientProxy.java
@@ -0,0 +1,26 @@
+package particleman;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.ModLoader;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class ClientProxy extends CommonProxy
+{
+    public static Minecraft mc;
+
+    public ClientProxy()
+    {
+        mc = ModLoader.getMinecraftInstance();
+    }
+
+    @Override
+    public void init(ParticleMan pMod)
+    {
+        super.init(pMod);
+        
+        //RenderingRegistry.registerEntityRenderingHandler(EntityScent.class, new RenderScent());
+    }
+    
+}
